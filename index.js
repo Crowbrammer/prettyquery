9221fe8c6498ab56e94b4e80f7857badc8843b63
Aaron Bell <aaronb@onyxut.com>
2020-10-01 13:34:26 -0400

    PQuery now supports mass inputs

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-mysql    = require('mysql');
+const mysql = require('mysql');
+
 class PQuery {
     constructor(options) {
         this.user       = options.user     || process.env.USER;
@@ -37,6 +38,69 @@ class PQuery {
         await this.query(`DROP DATABASE IF EXISTS ${dbName};`);
     }
 
+    insert(table, columns, values){
+        let sql = ''
+        let startingSQL = `INSERT INTO ${table} (`;
+        sql += startingSQL;
+        
+        // Add the columns
+        if (Array.isArray(columns)) {
+            for (let i = 0; i < columns.length; i++) {
+                const col = columns[i];
+                if (i < columns.length - 1) {
+                    sql += col + ', '
+                } else {
+                    sql += col + ')'
+                }
+            }
+        } else {
+            throw new Error('Under construction...')
+        }
+        
+        // Add the values
+        sql += ' VALUES '
+
+        // a single value
+        if (typeof values === 'string') {
+            sql +=  `('${values}')`;  
+        // multiple value
+        } else if (Array.isArray(values)) {
+            for (let i = 0; i < values.length; i++) {
+                const value = values[i];
+                // Iterate over the values...
+                // If it's an array of strings...
+                // esp. if there's only one column...
+                let newStr = '(';
+                if (typeof value === 'string' && columns.length === 1) {
+                    newStr += `'${value}')`;
+                } else if (typeof value === 'string' && columns.length === 1) {
+                    throw new Error('For inserts with more than one column, the array must contain arrays, not strings.')
+                } else if (Array.isArray(value)) {
+                    // Add each...
+                    for (let i = 0; i < value.length; i++) {
+                        const innerValue = value[i];
+                        if (i < value.length - 1) {
+                            newStr += `'${innerValue}'`  + ','
+                        } else {
+                            newStr += `'${innerValue}'` + ')'
+                        }
+                    }
+                }
+                
+                if (i < values.length - 1) {
+                    sql += newStr + ', '
+                } else {
+                    sql += newStr + ';' // It's the last one so clase the sql;
+                }
+                
+            }
+        } else {
+            throw new Error('values argument must be of type Array.');
+        }
+        return this.query(sql);
+
+    }
+
     async listAvailableDbs() {
         let rawDbs = await this.query('SHOW DATABASES;');
         let dbs = rawDbs.map(row => row.Database);
@@ -68,6 +132,8 @@ class PQuery {
         this.db = dbName; // Need typechecking.
         this.query(`USE ${dbName};`);
     }
+
+    
 }
 
 new PQuery({user: 'foo', password: 'bar'});
--- test/main.test.js
+++ test/main.test.js
@@ -20,6 +20,24 @@ describe('PQuery for MySQL', function () {
         }, 3, done);
     });
 
+    xit('Disconnects with shorthand', function (done) {
+        const pQuery = new PQuery({user: process.env.USER, password: process.env.PASSWORD});
+        expect(pQuery.connection.state).to.equal('disconnected');
+        pQuery.quit();
+        let intervals = 0;
+        const interval = setInterval(done => {
+            if (pQuery.authErrorThrown) {
+                done();
+                clearInterval(interval);
+            } else {
+                intervals++;
+            }
+            if (intervals > 30) {
+                expect('authError').to.equal('Never thrown');
+            }
+        }, 3, done);
+    });
+
     it('Returns a list of Dbs', async function () {
         const pQuery = new PQuery({ user: process.env.USER, password: process.env.PASSWORD });
         await pQuery.query('CREATE DATABASE IF NOT EXISTS test_db;');
@@ -75,10 +93,64 @@ describe('PQuery for MySQL', function () {
         await pQuery.createDb('test_db');
         expect(await pQuery.listAvailableDbs()).to.include('test_db');
         pQuery.connection.end();
-        
+
         const pQueryWithDb = new PQuery({user: process.env.DB_USER, password: process.env.DB_PASSWORD, db: 'test_db'})
         expect(await pQueryWithDb.showCurrentDb()).to.equal('test_db');
         pQueryWithDb.connection.end();
+    })
+
+    it('Inserts one thing into the moaning, ready-for-it DB', async function () {
+        // Set up
+        const pQuery = new PQuery({user: process.env.DB_USER, password: process.env.DB_PASSWORD})
+        await pQuery.query('DROP DATABASE IF EXISTS test_db;');
+        expect(await pQuery.listAvailableDbs()).to.not.include('test_db');
+        await pQuery.createDb('test_db');
+        await pQuery.useDb('test_db');
+        await pQuery.query('CREATE TABLE test (id INTEGER PRIMARY KEY AUTO_INCREMENT, foo VARCHAR(20));');
+
+        // Test single inserts.
+        await pQuery.insert('test', ['foo'], 'Wow');
+        let testValues = await pQuery.query('SELECT * FROM test;');
+        expect(testValues[0].foo).to.equal('Wow');
+        await pQuery.insert('test', ['foo'], ['Amazing']);
+        testValues = await pQuery.query('SELECT * FROM test;');
+        expect(testValues[1].foo).to.equal('Amazing');
+        pQuery.connection.end();
+    })
+
+    it('Mass inserts into the DB', async function () {
+        // Set up
+        const pQuery = new PQuery({user: process.env.DB_USER, password: process.env.DB_PASSWORD})
+        await pQuery.query('DROP DATABASE IF EXISTS test_db;');
+        expect(await pQuery.listAvailableDbs()).to.not.include('test_db');
+        await pQuery.createDb('test_db');
+        await pQuery.useDb('test_db');
+        await pQuery.query('CREATE TABLE test (id INTEGER PRIMARY KEY AUTO_INCREMENT, foo VARCHAR(20), bar VARCHAR(20));')
+
+        // Test mass inserts.
+        // Array of strings
+        await pQuery.insert('test', ['foo'], ['Wow', 'Amazing', 'Incredible']);
+        let testValues = await pQuery.query('SELECT * FROM test;');
+        testValues = testValues.map(value => value.foo);
+        expect(testValues).to.include.members(['Wow', 'Amazing', 'Incredible']);
+        
+        // Array of arrays
+        await pQuery.insert('test', ['foo'], [['Segoi'], ['Incroyable'], ['Ii desu ka?']]);
+        testValues = await pQuery.query('SELECT * FROM test;');
+        testValues = testValues.map(value => value.foo);
+        expect(testValues).to.include.members(['Wow', 'Amazing', 'Incredible']);
+        
+        await pQuery.insert(['test'], ['foo'], [['Segoi'], ['Incroyable'], ['Ii desu ka?']]);
+        testValues = await pQuery.query('SELECT * FROM test;');
+        testValues = testValues.map(value => value.foo);
+        expect(testValues).to.include.members(['Wow', 'Amazing', 'Incredible']);
+        
+        // Multiple columns, array of arrays
+        await pQuery.insert(['test'], ['foo', 'bar'], [['Fuck', 'you'], ['Without', 'me'], ['Bye', 'bye']]);
+        testValues = await pQuery.query('SELECT * FROM test;');
+        testValues = testValues.map(value => [value.foo, value.bar]);
+        expect(testValues).to.deep.include.members([['Fuck', 'you'], ['Without', 'me'], ['Bye', 'bye']]);
+        pQuery.connection.end();
     })
 
 })
